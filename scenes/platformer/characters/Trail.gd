8a7779ae8b4b6b4a04ea3cba5c939245616db447
Petra Andraschko <51335299+monochromia@users.noreply.github.com>
2022-05-03 13:20:39 -0400

    Missed a "texture =". Also did somebody's merge break bus recently? It exists both in the player and as a standalone I think.

--- scenes/platformer/characters/Trail.gd
+++ scenes/platformer/characters/Trail.gd
@@ -10,11 +10,10 @@ onready var player: Node2D = owner
 	
 
 func _ready():
-	randomize()
 	var result = FileUtils._getFilePathsFromImport(textures_directory, ".png", ".jpg")
 	var finalPath = result[randi() % result.size()]
 	if result:
-		ResourceLoader.load(finalPath, "Texture")
+		texture = ResourceLoader.load(finalPath, "Texture")
 
 func _process(_delta):
 	global_position = Vector2(0, 0)
